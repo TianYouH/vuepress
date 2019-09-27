bd99f25728d64424a1e7fe11a115b12b8b56b5a8
huangjinliang <huangjinliang@zto.cn>
2019-09-27 16:06:16 +0800

    初始化工程

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# VuePress 博客
-
-[官网](https://vuepress.docschina.org/)
-
-### 注意
-- 确保 vue@2.6.7 vue-template-compiler@2.6.7 一致，否则会编译失败
-  `yarn global add vue-template-compiler`
+# 你好世界
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  title: '个人主页', 
-  description: '黄金亮的博客',
-  head: [
-      ['link', { rel: 'icon', href: '/img/logo.png' }]
-  ]
+  title: 'Hello VuePress',
+  description: 'Just playing around'
 }
