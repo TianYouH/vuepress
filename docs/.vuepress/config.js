377da9197d845750dd89970b3927add6e4772f3c
huangjinliang <huangjinliang@zto.cn>
2019-09-27 17:50:48 +0800

    提交博客开发一版

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,16 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around'
+  title: '天幽的博客',
+  description: '我不是针对谁',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: '前端', link: '/web/' },
+      { text: '下拉测试',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: '11', link: '/language/chinese/' },
+          { text: '222', link: '/language/japanese/'}
+      ]}
+    ]
+  }
 }
