d326e6d2f45fe306e0344d52605f7b5411eef811
huangjinliang <huangjinliang@zto.cn>
2020-01-16 17:50:04 +0800

    更新博客

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,15 +3,16 @@ module.exports = {
   description: '我不是针对谁',
   themeConfig: {
     nav: [
-      { text: 'Home  ', link: '/' },
+      { text: '首页', link: '/' },
       { text: 'HTML5', link: '/html5/' },
       { text: 'JavaScript', link: '/javascript/' },
       { text: 'CSS', link: '/css/' }
     ],
-    sidebar: [
-      '/',
-      '/html5',
-      ['/javascript', 'Explicit link text']
-    ]
+    sidebar: {
+      '/html5/': [
+        ['', '基础'],
+        ['preload', '通过rel="preload"进行内容预加载']
+      ]
+    }
   }
 }
