4382fdd4e9fda3600c31520469755620e6d2d11e
HuangJinLiang <huangjinliang@zto.com>
2020-08-08 11:18:20 +0800

    增加侧边栏

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: '你好世界',
+    description: '这是我的博客',
+    themeConfig: {
+      nav: [
+        { text: '数学', link: '/math/' }
+      ],
+      sidebar: {
+          "/math/": [
+              ""
+          ]
+      }
+    }
+  }
