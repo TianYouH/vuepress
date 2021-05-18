b2e40ca6065b579f258d72b882cba72bfad38a78
DESKTOP-ER5718D\zt <huangjinliang@zto.com>
2021-05-18 17:54:06 +0800

    feat: 测试chai.js完善

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,15 +10,26 @@ module.exports = {
     description: '这是我的博客',
     themeConfig: {
       nav: [
-        { text: 'HTML', link: '/html/' },
-        { text: 'Javascript', link: '/javascript/' },
-        { text: 'CSS', link: '/css/' },
-        { text: 'WebApi', link: '/webapi/' },
-        { text: 'HTTP', link: '/http/' },
-        { text: 'Webpack', link: '/webpack/' },
-        { text: 'GIT', link: '/git/' },
-        { text: 'Linux', link: '/linux/' },
-        { text: '知识点', link: '/knowledge/' },
+        {
+            text: '前端',
+            items: [
+                { text: 'HTML', link: '/html/' },
+                { text: 'Javascript', link: '/javascript/' },
+                { text: 'CSS', link: '/css/' },
+                { text: 'WebApi', link: '/webapi/' },
+                { text: 'Webpack', link: '/webpack/' },
+                { text: '知识点', link: '/knowledge/' },
+            ]
+        },
+        {
+            text: '开发',
+            items: [
+                { text: '测试', link: '/test/' },
+                { text: 'GIT', link: '/git/' },
+                { text: 'HTTP', link: '/http/' },
+                { text: 'Linux', link: '/linux/' },
+            ]
+        },
       ],
       sidebarDepth: 3,
       sidebar: {
@@ -39,32 +50,37 @@ module.exports = {
           "/webapi/": [
               ""
           ],
-          "/http/": [
-              ""
-          ],
           "/webpack/": [
-              "",
-              "搭建Webpack环境",
-              "使用Webpack的配置文件",
-              "Loader",
-              "Plugins",
-              "CodeSplitting",
-          ],
-          "/git/": [
-              "",
-              "Git-Commit-message和Change-log验证配置",
-          ],
-          "/linux/": [
-              ""
-          ],
-          "/knowledge/": [
-              "",
-              "安全",
-              "常用手写函数",
-              "初级高频面试题",
-              "性能优化",
-              "运行环境",
-          ],
+            "",
+            "搭建Webpack环境",
+            "使用Webpack的配置文件",
+            "Loader",
+            "Plugins",
+            "CodeSplitting",
+        ],
+        "/knowledge/": [
+            "",
+            "安全",
+            "常用手写函数",
+            "初级高频面试题",
+            "性能优化",
+            "运行环境",
+        ],
+
+        "/test/": [
+            "",
+            "Chaijs"
+        ],
+        "/git/": [
+            "",
+            "Git-Commit-message和Change-log验证配置",
+        ],
+        "/http/": [
+            ""
+        ],
+        "/linux/": [
+            ""
+        ]
       }
     }
   }
---
+++ docs/test/README.md
@@ -0,0 +1,4 @@
+# test
+
+[Chai.js 断言库](https://www.chaijs.com/)
+
